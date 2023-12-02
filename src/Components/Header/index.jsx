1529b478ca355a6f90cb3a31eeaebce5a53c4d63
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-12-02 08:35:27 -0800

    list is working

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,8 +1,9 @@
 import { useContext } from 'react';
-import { Button, Box } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { GlobalContext } from '../../App';
+import { Link } from 'react-router-dom';
 
 const index = ({ incomplete }) => {
   const { toggleAppTheme, appTheme } = useContext(GlobalContext);
@@ -13,13 +14,27 @@ const index = ({ incomplete }) => {
   };
 
   return (
-    <Box sx={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      padding: '1rem',
-      backgroundColor: '#f5f5f5',
-    }}>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem',
+        backgroundColor: '#f5f5f5',
+      }}
+    >
+      <Link to={'/'}>
+        <Typography variant="h4" gutterBottom style={{ margin: 10 }}>
+          Home
+        </Typography>
+      </Link>
+
+      <Link to={'/settings'}>
+        <Typography variant="h4" gutterBottom style={{ margin: 10 }}>
+          Settings
+        </Typography>
+      </Link>
+
       <h1 data-testid="todo-h1">
         To Do List: {incomplete.length} items pending
       </h1>
